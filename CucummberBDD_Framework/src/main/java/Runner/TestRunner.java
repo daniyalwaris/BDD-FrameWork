09c827035d38d82172a73c48a5dd96abf471b323
Daniyal Waris <daniyal.waris@yahoo.com>
2020-11-23 23:32:49 +0100

    Added New File and Attributes in the runner file

--- CucummberBDD_Framework/src/main/java/Runner/TestRunner.java
+++ CucummberBDD_Framework/src/main/java/Runner/TestRunner.java
@@ -8,8 +8,11 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = "D:\\daniyalwaris\\BDD-FrameWork\\CucummberBDD_Framework\\src\\main\\java\\Feature\\Successful_Payment.feature",
-		glue= {"StepScript"}
+		features = "D:\\daniyalwaris\\BDD-FrameWork\\CucummberBDD_Framework\\src\\main\\java\\Feature\\Successful_Payment.feature", //Path of the feature File
+		glue= {"StepScript"}, 						//Path of the Script File
+		plugin= {"pretty","html:test-output","json:json_output/cucumber.json","junit:junit_xml/cucumber.xml"}, // In order to generate the output file for the system.
+		strict = true,   // This attricbute will map from the Feature File
+		monochrome = true 							//Display the concole output in a proper readable format
 		
 		)
 
