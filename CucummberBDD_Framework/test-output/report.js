dbbe64f18e442a3aa5e058b595ec13c6d9834ce6
Daniyal Waris <daniyal.waris@yahoo.com>
2020-11-27 03:24:33 +0100

    Update

--- CucummberBDD_Framework/test-output/report.js
+++ CucummberBDD_Framework/test-output/report.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("D:/daniyalwaris/BDD-FrameWork/CucummberBDD_Framework/src/main/java/Feature/Successful_Payment.feature");
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("D:/daniyalwaris/BDD-FrameWork/CucummberBDD_Framework/src/main/java/Feature/Fail_Payment.feature");
 formatter.feature({
   "line": 1,
   "name": "UI automation framework",
@@ -7,73 +7,87 @@ formatter.feature({
   "keyword": "Feature"
 });
 formatter.scenario({
-  "line": 3,
-  "name": "Test script to test end to end successful checkout flow for purchasing",
+  "comments": [
+    {
+      "line": 3,
+      "value": "#---------------------------------------------------------------------------#"
+    },
+    {
+      "line": 4,
+      "value": "# In case of Example we will us Scenario Outline"
+    }
+  ],
+  "line": 6,
+  "name": "Test Unsuccesful end to end checkout flow due expire card details and Show the Output",
   "description": "",
-  "id": "ui-automation-framework;test-script-to-test-end-to-end-successful-checkout-flow-for-purchasing",
+  "id": "ui-automation-framework;test-unsuccesful-end-to-end-checkout-flow-due-expire-card-details-and-show-the-output",
   "type": "scenario",
   "keyword": "Scenario"
 });
 formatter.step({
-  "line": 4,
-  "name": "User is on the purchase page",
+  "line": 8,
+  "name": "User is on the webPage For buying pillows",
   "keyword": "Given "
 });
 formatter.step({
-  "line": 5,
-  "name": "title of page is coco",
+  "line": 9,
+  "name": "title of page is Sample Store",
   "keyword": "When "
 });
 formatter.step({
-  "line": 6,
-  "name": "User able to proceed with the checkout flow for purchasing Pillow",
+  "line": 10,
+  "name": "User able to proceed with the payment checkout",
   "keyword": "Then "
 });
 formatter.step({
-  "line": 7,
-  "name": "User able to purchase though the Credit card payment method and Enter the \"4811111111111114\" and \"0221\" and \"123\"",
+  "line": 11,
+  "name": "User able to enter the Credit card details",
+  "rows": [
+    {
+      "cells": [
+        "CerditCardNumber",
+        "Expirydate",
+        "OPT"
+      ],
+      "line": 12
+    },
+    {
+      "cells": [
+        "4811111111111114",
+        "0221",
+        "123"
+      ],
+      "line": 13
+    }
+  ],
   "keyword": "Then "
 });
 formatter.match({
-  "location": "SuccessfulPaymentScript.webpage()"
+  "location": "FailurePayment.webpage()"
 });
 formatter.result({
-  "duration": 8175679300,
+  "duration": 8018030700,
   "status": "passed"
 });
 formatter.match({
-  "location": "SuccessfulPaymentScript.webpage_validation()"
+  "location": "FailurePayment.webpage_validation()"
 });
 formatter.result({
-  "duration": 74729000,
+  "duration": 89504900,
   "status": "passed"
 });
 formatter.match({
-  "location": "SuccessfulPaymentScript.customer_checkout()"
+  "location": "FailurePayment.customer_checkout()"
 });
 formatter.result({
-  "duration": 437346200,
+  "duration": 312400300,
   "status": "passed"
 });
 formatter.match({
-  "arguments": [
-    {
-      "val": "4811111111111114",
-      "offset": 75
-    },
-    {
-      "val": "0221",
-      "offset": 98
-    },
-    {
-      "val": "123",
-      "offset": 109
-    }
-  ],
-  "location": "SuccessfulPaymentScript.customercheckout_paymentMethod(String,String,String)"
+  "location": "FailurePayment.customercheckout_paymentMethod(DataTable)"
 });
 formatter.result({
-  "duration": 3170863100,
+  "duration": 3040091500,
   "status": "passed"
 });
 });
